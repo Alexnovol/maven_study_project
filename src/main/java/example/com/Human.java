936dd2426410cb4f4e13f401b90484775b9209ea
Alexnovol <alexxnovol@icloud.com>
2024-04-14 11:24:18 +0300

    Initial commit

---
+++ src/main/java/example/com/Human.java
@@ -0,0 +1,13 @@
+package example.com;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Human {
+    String firstname;
+    String lastname;
+}
---
+++ src/main/java/example/com/Main.java
@@ -0,0 +1,8 @@
+package example.com;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human = new Human("James", "Gosling");
+        System.out.println(human);
+    }
+}
